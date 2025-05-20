69bc5f28f4fa64033842a5ac8d4632303f8d8260
KawaiiBakemono <kathryn.beam@kawaiibakemono.art>
2025-05-20 13:50:30 -0700

    Improved readability/design on Waking Dreams page

--- js/effects.js
+++ js/effects.js
@@ -1,8 +1,8 @@
 function moveableBackground() {
-    const background = document.getElementById('moveable-background');
+    const background = document.getElementById("moveable-background");
     const sensitivity = 15;
 
-    document.addEventListener('mousemove', function(el) {
+    document.addEventListener("mousemove", function(el) {
         var x = el.clientX / sensitivity;
         var y = el.clientY / sensitivity;
         background.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
@@ -10,6 +10,8 @@ function moveableBackground() {
 }
 
 moveableBackground();
+
+// ======== Clickable Aina ========
 
 var i = 0;
 
@@ -24,30 +26,60 @@ function changeImage() {
     }
 }
 
-function clickExplosion(tag, onfire, explode) {
-    const element = document.getElementById(tag);
-    const fireOverlay = document.getElementById(onfire);
-    const explosionOverlay = document.getElementById(explode);
-    const count = element.dataset.counter;
+// ======== CLICK EXPLOSION ========
 
-    if (count == 20) {
-        fireOverlay.style.visibility = 'visible';
-        document.getElementById('setfire-audio').play();
-        document.getElementById('onfire-audio').play();
+function clickExplosion(_parent, _image) {
+    const parent = document.getElementById(_parent);
+    const baseImage = document.getElementById(_image);
+    const count = parent.dataset.counter;
+
+    if (count == 25) {
+        createImgElement(parent, "display-img-medium img-fire", "/img/onfire.gif");
+        createAudioElement(parent, "/audio/flash_fire_ignite.ogg", false);
+        createAudioElement(parent, "/audio/on_fire.wav", true);
     } 
     else if (count == 50) {
-        explosionOverlay.style.visibility = 'visible';
-        element.style.visibility = 'hidden';
-        document.getElementById('explosion-audio').play();
+        createImgElement(parent, "display-img-medium img-explosion", "/img/the_explosion.gif", "explosion");
+        createAudioElement(parent, "/audio/deltarune-explosion.ogg", false);
+        baseImage.style.visibility = "hidden";
+        parent.style.pointerEvents = "none";
         setTimeout(() => {
-            explosionOverlay.style.visibility = 'hidden';
-        }, "600");
+            parent.removeChild(document.getElementById("explosion"));
+        }, "900");
     }
-    Number(element.dataset.counter++);
+    Number(parent.dataset.counter++);
     console.log(count)
 }
 
+function createImgElement(_parent, _class, _file, _id = "") {
+    let img = document.createElement("img");
+    img.className = _class;
+    img.src = _file;
+    if (_id != "") {
+        img.id = _id;
+    }
+    _parent.appendChild(img);
+}
+
+function createAudioElement(_parent, _file, _loop) {
+    let audio = document.createElement("audio");
+    if (_loop) {
+        audio.loop = true;
+    }
+    audio.textContent = "Your browser doesn't support the audio element. Womp womp.";
+    _parent.appendChild(audio);
+    
+    let source = document.createElement("source");
+    source.src = _file;
+    let extension =  _file.split(".").pop();
+    source.type = `audio/${extension}`;
+    audio.appendChild(source);
+    
+    audio.play();
+}
+
 // ======== MODAL ========
+
 var modal = document.getElementById("modal");
 var modalImg = document.getElementById("modal-img");
 var captionText = document.getElementById("caption");
